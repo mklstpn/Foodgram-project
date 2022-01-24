97e366d02afcd5eeee03c03e084cf21ce30a4c1e
Mikhail Stepanov <slake6@yandex.ru>
2022-01-25 01:32:59 +0300

    fixes

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -5,7 +5,7 @@ class Ingredient(models.Model):
     name = models.CharField(
         max_length=255, verbose_name='Название ингредиента')
     measurement_unit = models.CharField(
-        max_length=10, verbose_name='Единицы измерения')
+        max_length=255, verbose_name='Единицы измерения')
 
     class Meta:
         ordering = ('name', )
