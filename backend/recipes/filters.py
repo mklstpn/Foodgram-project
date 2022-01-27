031d30289a147391ee6f92e0e1d0176fb42007df
Mikhail Stepanov <slake6@yandex.ru>
2022-01-27 20:08:53 +0300

    fixes

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -33,7 +33,7 @@ class RecipeFilter(FilterSet):
 
     def get_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value == 1:
-                return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
